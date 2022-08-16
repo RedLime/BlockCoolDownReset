b3bbbc709164d836b138ee35a65e2461ccbcd3ee
RedLime <redlimerl@gmail.com>
2022-08-16 21:27:40 +0900

    1.2

--- src/main/java/com/redlimerl/mc/ihateitemcooldown/mixins/MixinClientPlayNetworkHandler.java
+++ src/main/java/com/redlimerl/mc/ihateitemcooldown/mixins/MixinClientPlayNetworkHandler.java
@@ -3,21 +3,21 @@ package com.redlimerl.mc.ihateitemcooldown.mixins;
 import com.redlimerl.mc.ihateitemcooldown.BreakCoolDownReset;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayNetworkHandler.class)
+@Mixin(InGameHud.class)
 public class MixinClientPlayNetworkHandler {
 
-    @Shadow private MinecraftClient client;
+    @Shadow @Final private MinecraftClient client;
 
-    @Redirect(method = "onTitle", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;setOverlayMessage(Lnet/minecraft/text/Text;Z)V"))
-    public void onTitleMixin(InGameHud instance, Text message, boolean tinted) {
-        instance.setOverlayMessage(message, tinted);
+    @Inject(method = "setOverlayMessage", at = @At("HEAD"))
+    public void onTitleMixin(Text message, boolean tinted, CallbackInfo ci) {
         if (message.getString().isEmpty()) {
             if (client.interactionManager != null)
                 ((AccessorClientPlayerInteractionManager) client.interactionManager).setBlockBreakCd(BreakCoolDownReset.COOL_DOWN);
