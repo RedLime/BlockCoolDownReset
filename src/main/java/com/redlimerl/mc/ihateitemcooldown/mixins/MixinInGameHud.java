ec4476688bf2c760fa379eb27c72f42a0d21706d
qwertyuioplkjhgfd <charles07023@gmail.com>
2022-09-10 20:37:16 -0700

    git is very confused

--- src/main/java/com/redlimerl/mc/ihateitemcooldown/mixins/MixinInGameHud.java
+++ src/main/java/com/redlimerl/mc/ihateitemcooldown/mixins/MixinInGameHud.java
@@ -2,21 +2,23 @@ package com.redlimerl.mc.ihateitemcooldown.mixins;
 
 import com.redlimerl.mc.ihateitemcooldown.BreakCoolDownReset;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.packet.s2c.play.PlaySoundIdS2CPacket;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayNetworkHandler.class)
-public class MixinClientPlayNetworkHandler {
-    @Shadow private MinecraftClient client;
+@Mixin(InGameHud.class)
+public class MixinInGameHud {
 
-    @Inject(method = "onPlaySoundId", at = @At("TAIL"))
-    public void onPlaySoundIdMixin(PlaySoundIdS2CPacket packet, CallbackInfo ci) {
-        if (packet.getSoundId().toString().equals("cdreset:reset_cooldown")) {
+    @Shadow @Final private MinecraftClient client;
+
+    @Inject(method = "setOverlayMessage", at = @At("HEAD"))
+    public void onTitleMixin(Text message, boolean tinted, CallbackInfo ci) {
+        if (message.getString().isEmpty()) {
             if (client.interactionManager != null)
                 ((AccessorClientPlayerInteractionManager) client.interactionManager).setBlockBreakCd(BreakCoolDownReset.COOL_DOWN);
         }
